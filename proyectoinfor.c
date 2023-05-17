2ffe25a06a695316f9a414106a7cff73e4e70cad
Luissalass2204 <132624332+Luissalass2204@users.noreply.github.com>
2023-05-16 21:04:23 -0500

    Update proyectoinfor.c

--- proyectoinfor.c
+++ proyectoinfor.c
@@ -1,54 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int terminar(void);
 
 int main(int argc, const char *argv[]){
 
     int end = 1;
     int opc = 0;
     int sub_opc = 0;
-    int sub_opc2 = 0;
+    int sub_opc2 = 0;               //variables
     int sub_opc2cuhidr = 0;
     int sub_opc3 = 0;
     int sub_opc3Sol = 0;
+    int salir = 0;
     
     while(end){
         printf("Hola, hoy miraremos el estado de las cuencas hidrográficas en Popayán.\n");
         printf("Seleccione una opción del menú:\n");
         printf("Menú.\n");
         printf("[1]Estado de las cuencas hidrograficas\n");
-        printf("[2]Cuencas hidrograficas en deterioro\n");     
+        printf("[2]Cuencas hidrograficas en deterioro\n");        //Menu principal
         printf("[3]Cuidado de las cuencas en deterioro\n");
         printf("[4]Terminar y salir del programa.\n");
         
         scanf("%d",&opc);
         //system (cls);
-        printf("Selecciionaste la opción %c.\n", &opc);
+        printf("Selecciionaste la opción %d.\n", &opc);
         
         if (opc == 1){
             printf("Selecciona la cuenca que le interesa:\n ");
             printf("\tOpciones.\n");
             printf("[1]Cuenca 1.\n");
-            printf("[2]Cuenca 2.\n");        
+            printf("[2]Cuenca 2.\n");                      //menu de la opción 1
             printf("[3]Cuenca 3.\n");
             printf("[4]Cuenca 4.\n");
             
             scanf("%d",&sub_opc);
 
             if (sub_opc == 1){
-                printf("Información de la cuenca 1.\n");
+                printf("Informacion de la cuenca 1.\n");
+                end = terminar();
             } else if (sub_opc == 2){
                 printf("Información de la cuenca 2.\n");
+                end = terminar();
             } else if (sub_opc == 3){
                 printf("Información de la cuenca 3.\n");
+                end = terminar();
             } else if (sub_opc == 4){
                 printf("Información de la cuenca 4.\n");
+                end = terminar();
             }
         } else if (opc == 2){
             printf("Seleccione la cuenca hodrografica en deterioro sobre la que quiere saber:\n");
             printf("[1]Cuenca 1.\n");
             printf("[2]Cuenca 2.\n");        
-            printf("[3]Cuenca 3.\n");
+            printf("[3]Cuenca 3.\n");            //menu opción 2
             printf("[4]Cuenca 4.\n");
 
             scanf("%d",&sub_opc2);
@@ -63,8 +69,10 @@ int main(int argc, const char *argv[]){
 
                 if (sub_opc2cuhidr == 1){
                     printf("Escribir la causa 1\n");
+                    end = terminar();
                 } else if (sub_opc2cuhidr == 2){
                     printf("Escribir la causa 2\n");
+                    end = terminar();
                 }
             } else if (sub_opc2 == 2){
                 printf("La cuenca 2 se encuentra en deterioro principalmente por las siguientes 2 causas, causa 1 y causa 2.\n");
@@ -75,9 +83,11 @@ int main(int argc, const char *argv[]){
                 scanf("%d",&sub_opc2cuhidr);
 
                 if (sub_opc2cuhidr == 1){
-                    printf("Escribir la causa 1\n");
+                    printf("Escribir la causa 1.\n");
+                    end = terminar();
                 } else if (sub_opc2cuhidr == 2){
-                    printf("Escribir la causa 2\n");
+                    printf("Escribir la causa 2.\n");
+                    end = terminar();
                 }
             } else if (sub_opc2 == 3){
                 printf("La cuenca 3 se encuentra en deterioro principalmente por las siguientes 2 causas, causa 1 y causa 2.\n");
@@ -89,8 +99,10 @@ int main(int argc, const char *argv[]){
 
                 if (sub_opc2cuhidr == 1){
                     printf("Escribir la causa 1\n");
+                    end = terminar();
                 } else if (sub_opc2cuhidr == 2){
                     printf("Escribir la causa 2\n");
+                    end = terminar();
                 }
             } else if (sub_opc2 == 4){
                 printf("La cuenca 4 se encuentra en deterioro principalmente por las siguientes 2 causas, causa 1 y causa 2.\n");
@@ -102,15 +114,17 @@ int main(int argc, const char *argv[]){
 
                 if (sub_opc2cuhidr == 1){
                     printf("Escribir la causa 1");
+                    end = terminar();
                 } else if (sub_opc2cuhidr == 2){
                     printf("Escribir la causa 2");
+                    end = terminar();
                 }
             }
         } else if (opc == 3){
             printf("Seleccione la cuenca sobre la que quiere saber su cuidado.\n");
             printf("[1]Cuenca 1.\n");
             printf("[2]Cuenca 2.\n");        
-            printf("[3]Cuenca 3.\n");
+            printf("[3]Cuenca 3.\n");                  //menu opción 3
             printf("[4]Cuenca 4.\n");
 
             scanf("%d",&sub_opc3);
@@ -125,8 +139,10 @@ int main(int argc, const char *argv[]){
 
                 if (sub_opc3Sol == 1){
                     printf("Escribir la solución 1\n");
+                    end = terminar();
                 } else if (sub_opc3Sol == 2){
                     printf("Escribir la solución 2\n");
+                    end = terminar();
                 }
             } else if (sub_opc3 == 2){
                 printf("La cuenca 2 puede estar mejor con las siguientes soluciones, solución 1 y solución 2.\n");
@@ -138,8 +154,10 @@ int main(int argc, const char *argv[]){
 
                 if (sub_opc3Sol == 1){
                     printf("Escribir la solución 1\n");
+                    end = terminar();
                 } else if (sub_opc3Sol == 2){
                     printf("Escribir la solución 2\n");
+                    end = terminar();
                 }
             } else if (sub_opc3 == 3){
                 printf("La cuenca 3 puede estar mejor con las siguientes soluciones, solución 1 y solución 2.\n");
@@ -151,8 +169,10 @@ int main(int argc, const char *argv[]){
 
                 if (sub_opc3Sol == 1){
                     printf("Escribir la solución 1.\n");
+                    end = terminar();
                 } else if (sub_opc3Sol == 2){
                     printf("Escribir la solución 2.\n");
+                    end = terminar();
                 }
             } else if (sub_opc3 == 4){
                 printf("La cuenca 4 puede estar mejor con las siguientes soluciones, solución 1 y solución 2.\n");
@@ -164,15 +184,33 @@ int main(int argc, const char *argv[]){
 
                 if (sub_opc3Sol == 1){
                     printf("Escribir la solución 1.\n");
+                    end = terminar();
                 } else if (sub_opc3Sol == 2){
                     printf("Escribir la solución 2.\n");
+                    end = terminar();
                 }
             }
         } else if (opc == 4){
-            printf("Decidiste terminar con el programa, muchas gracias.\n");
+            end = terminar();
         }
         // end = 0
     }
         //system("pause");
         return 0;    
+}
+
+int terminar(){
+    int salir = 0;
+    int end = 1;
+    printf("¿Desea terminar?.\n");
+    printf("[1]Si.\n");
+    printf("[2]No.\n");
+
+    scanf("%d",&salir);
+    if (salir == 1) {
+        end=0;
+    } else if (salir == 2) {
+        end = 1;
+    }
+    return end;
 }
